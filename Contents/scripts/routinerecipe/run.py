b4029b31c641ebe9158669a94871368339fb59d8
Hum9183 <hum9183@gmail.com>
2023-12-23 13:09:42 +0900

    不要コメントを削除

--- Contents/scripts/routinerecipe/run.py
+++ Contents/scripts/routinerecipe/run.py
@@ -46,44 +46,6 @@ def run_recipe(flow_scene: FlowScene):
     __run_rr_script()
 
 
-# def __generate_source(start_node: Node) -> str:
-#     result: str = dedent('''\
-#         # -*- coding: utf-8 -*-
-#         from maya import cmds
-#         ''')
-#     result = __add_ln(result)
-#     result = __add_ln(result)
-#     result += 'def main():'
-#     result = __add_ln_indent(result)
-#
-#     node_state: NodeState = start_node.state
-#     connections: list[Connection] = node_state.output_connections
-#
-#     if connections == []:
-#         raise RoutineRecipeError(u'Startノードに接続がありません')
-#
-#     connection: Connection = connections[0]     # とりあえず決め打ちで1つ目だけ
-#     input_node: Node = connection.input_node
-#     result += input_node.source_code()
-#     result = __add_ln(result)
-#     return result
-
-
-# def __add_ln(source: str) -> str:
-#     return f'{source}\n'
-#
-#
-# def __add_ln_indent(source: str) -> str:
-#     return f'{source}\n    '
-
-
-# def __write(source: str):
-#     path = r'C:\Program Files\Autodesk\ApplicationPlugins\RoutineRecipe\Contents\scripts\routinerecipe\rr_main.py'
-#     with open(path, mode='w') as f:
-#         f.write(source)
-#     print(u'rr_main.pyを書き換えました')
-
-
 def __run_rr_script():
     importlib.reload(rr_main)
     rr_main.main()
